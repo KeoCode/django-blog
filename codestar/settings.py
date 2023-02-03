495f50f021309d21a86dd5600ebfa6162615dc5a
KeoCode <keocode101@gmail.com>
2023-02-03 21:34:58 +0000

    edited allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["codestar-blog-1", 'localhost']
+ALLOWED_HOSTS = ["/codestar-blog-1.herokuapp.com", 'localhost']
 
 
 # Application definition
